588071b01c111ecd666bef8c67b984e6da021134
YO <keneth.javier90@gmail.com>
2024-09-26 12:11:55 -0600

    se actualiza la rama

--- components/proyect/proeyect.js
+++ components/proyect/proeyect.js
@@ -1,25 +1,49 @@
 import { header } from "../header/header.js";
+import { dataProyectos } from "../../data/data.js";
 
+function proyectos() {
+    let section = document.createElement('section');
+    section.appendChild(header());
+    section.appendChild(listaProyectos());  // Corregir la llamada a listaProyectos
+    section.className = 'section-proyectos';
 
-function proyectos(){
-     let section = document.createElement('section');
-     section.appendChild(header())
-     section.appendChild(item())
-     section.className = 'section-proyectos';
-     
-
-
-     return section;
+    return section;
 }
 
-// item lista
-
-function item(){
+// Lista de proyectos
+function listaProyectos() {
     let div = document.createElement('div');
-    div.innerText = "item listo"
-    div.className = 'item';
+    div.classList = "lista-proyectos";
+
+    // Ajustamos el forEach para pasar correctamente los datos de cada proyecto
+    dataProyectos.forEach((proyecto) => {
+        div.appendChild(item(proyecto.nombre,proyecto.github,proyecto.stacks,proyecto.web));  // Pasamos el objeto completo
+    });
 
     return div;
 }
+function item(texto,github,githubpage,web,stacks){
+    let div = document.createElement('div');
+    div.className ="div-item"
+    
+    let a = document.createElement('a');
+    a.className= "link-githubPage"
+    a.href = githubpage;
+    a.innerText = "githubPage"
+    div.appendChild(a);
+
+    let stack = document.createElement('div');
+    stack.innerHTML = "[js,css,html]";
+    div.appendChild(stack);
+
+    let btnGithub = document.createElement('a');
+    btnGithub.className = "btn-github";
+    btnGithub.href = github;
+    btnGithub.innerText = "Github";
+    div.appendChild(btnGithub);
+
+    return div;
+}
+
 
 export {proyectos}
---
+++ data/data.js
@@ -0,0 +1,9 @@
+let dataProyectos = [{
+    nombre: "formularios",
+    descripcion: "tex texto texto",
+    githubpage:"https:/gogle}",
+    github:"https://github.com/keneth778/proyecto-formularios",
+    stacks:"ccs, js,html"
+}];
+
+export {dataProyectos}
